4c9e0c70e7f28d772e8fef7952af1df8dcbefb37
roma <silvervaza@gmail.com>
2019-07-14 22:07:18 +0300

    this game brain-even is working but inexplicitly what is packet which have to set up(global)

--- src/check-fairness.php
+++ src/check-fairness.php
@@ -8,19 +8,27 @@ function yesOrNo()
     line('Answer "yes" if number even otherwise answer "no".');
     $name = prompt('May I have your name? ');
     line("Hello , %s!", $name);
-    $numberOfTries = 3;
-    while ($numberOfTries <= 3) {
+    $counterRightResponse = 3;
+    $variantsAnswer = ["yes","no"];
+    while ($counterRightResponse > 0) {
 	    $random = rand();
-	    $correctResponse = 0;
-	    if ($random % 2 === 0) {
-		    $correctResponse = "yes";
-	    } else {
-		    $correctResponse ="no";
-	    }
+	    $correctResponse = $random % 2 === 0 ? "yes" : "no";
 	    line("Question: {$random}");
 	    $answerUser = prompt("Your answer");
-	    if ($answerUser === 'yes') {
-		    line("good good");
+	    if (in_array($answerUser, $variantsAnswer)) {
+		    if ($answerUser === $correctResponse) {
+			    $counterRightResponse--;
+			    line("Correct!");
+		    } else {
+			    line("{$answerUser} is wrong answer ;(. Correct answer was {$correctResponse}. Let's try again, {$name}!");
+			    break;
+		    }
+	    } else {
+		    line("Answer can only no or yes");
+		    break;
+	    }
+	    if ($counterRightResponse === 0) {
+		    line("Congratulations, {$name}!");
 	    }
     }
 }
