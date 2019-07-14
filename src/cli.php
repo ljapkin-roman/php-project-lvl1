ad69682606477ed98455c7f189f0f97b6ae438b5
roma <silvervaza@gmail.com>
2019-07-14 12:34:55 +0300

    add travis yml

--- src/cli.php
+++ src/cli.php
@@ -2,10 +2,9 @@
   namespace BrainGames\Cli;
   use function \cli\line;
   use function \cli\prompt;
-  function run()
-  {
-      line("Welcome to the Brain Game!");
-      $name = prompt('May i have your name');
-      line("Hello, %s!", $name);
-  }
-?>
+function run()
+{
+    line("Welcome to the Brain Game!");
+    $name = prompt('May i have your name');
+    line("Hello, %s!", $name);
+}
