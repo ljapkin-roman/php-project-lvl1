e1cfd9d602258da218cc17f8c9c4dff10b53f228
roma <silvervaza@gmail.com>
2019-07-20 20:56:04 +0300

    progression is work

--- src/games/progression.php
+++ src/games/progression.php
@@ -2,12 +2,27 @@
 namespace BrainGames\Progression;
 use function \cli\line;
 use function \cli\prompt;
-function progression() {
+function progression($nameUser) {
+    $correctAnswer = 0;
     $sizeIncrementalStep = rand(1, 10);
     $startPoint = rand(1,100);
     $progresValues = "";
     for ($i = 1;$i < 11;$i++) {
-        $progresValues .= ": ". $startPoint + $i * $sizeIncrementalStep;	
+	if ($sizeIncrementalStep !== $i) {
+            $progresValues .= " " .($startPoint + $i * $sizeIncrementalStep);	
+	} else {
+		$correctAnswer = $startPoint + $i * $sizeIncrementalStep;
+		$progresValues .=" ". ".." . " ";
+	}
     }
-    print_r($progresValues);
+    line("Question :"."{$progresValues}\n");
+    $userAnswer = intval(prompt("Your answer"));
+    if ($userAnswer === $correctAnswer) {
+	    line("Correct!\n");
+	    return true;
+    } else {
+        line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+        line("Let's try again {$nameUser}");
+        return false;
+    }
 }
