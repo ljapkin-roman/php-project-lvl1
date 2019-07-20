13e07e4e982d1fc0ca1d941bd86a2e2c9716686d
roma <silvervaza@gmail.com>
2019-07-20 22:07:18 +0300

    use sniffer

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 sample install package - https://asciinema.org/a/pJUcYtmsJEVKZrtCKkL7uxoPp
 install brain-calc and launch one - https://asciinema.org/a/w7x7DIOjCbjKoRjkxkh0iUszc
 install brain-gcd and launch one - https://asciinema.org/a/cQBlHwEThruSiLtDErgvibRA4
+install brain-progression and launch one - https://asciinema.org/a/sfUMATs8mvL7LNgzIHrFfS5Lx
--- src/games/progression.php
+++ src/games/progression.php
@@ -2,24 +2,25 @@
 namespace BrainGames\Progression;
 use function \cli\line;
 use function \cli\prompt;
-function progression($nameUser) {
+function progression($nameUser)
+{
     $correctAnswer = 0;
     $sizeIncrementalStep = rand(1, 10);
-    $startPoint = rand(1,100);
+    $startPoint = rand(1, 100);
     $progresValues = "";
     for ($i = 1;$i < 11;$i++) {
-	if ($sizeIncrementalStep !== $i) {
-            $progresValues .= " " .($startPoint + $i * $sizeIncrementalStep);	
-	} else {
-		$correctAnswer = $startPoint + $i * $sizeIncrementalStep;
-		$progresValues .=" ". ".." . " ";
-	}
+        if ($sizeIncrementalStep !== $i) {
+            $progresValues .= " " .($startPoint + $i * $sizeIncrementalStep);    
+        } else {
+            $correctAnswer = $startPoint + $i * $sizeIncrementalStep;
+            $progresValues .=" ". ".." . " ";
+        }
     }
     line("Question :"."{$progresValues}\n");
     $userAnswer = intval(prompt("Your answer"));
     if ($userAnswer === $correctAnswer) {
-	    line("Correct!\n");
-	    return true;
+        line("Correct!\n");
+        return true;
     } else {
         line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
         line("Let's try again {$nameUser}");
