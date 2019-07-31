cbd6605751c2159083f142dfa0f61e27e78b8ac0
roma <silvervaza@gmail.com>
2019-07-31 22:48:38 +0300

    lalka rewrite at all.Yes all

--- src/games/even.php
+++ src/games/even.php
@@ -10,29 +10,16 @@ function isEven($number)
 	}
 	return false;
 }
-function yesOrNo($nameUser)
+function yesOrNo()
 {
     $variantsAnswer = ["yes","no"];
+    $rules = 'Answer "yes" if number even otherwise answer "no".';
+    $quest = "is {$enigmaNumber} even?";
         $enigmaNumber = rand();
         $correctResponse =  isEven($enigmaNumber) ? "yes" : "no";
-        line("Question: {$enigmaNumber}");
-        $answerUser = prompt("Your answer");
-	if (!in_array($answerUser, $variantsAnswer)) {
-	     line("Answer can only no or yes!");
-	     return false;
-	}
 
-        if ($answerUser !== $correctResponse) {
-     line("\"{$answerUser}\" is wrong answer ;l(. Correct answer was \"{$correctResponse}\". 
-			Let's try again, {$nameUser}!");
-            return false;
-
-        }
-	line('Correct');
-	return true;
 }
 function launch()
 {
-    $rules = 'Answer "yes" if number even otherwise answer "no".';
     engine('Src\Games\Even\yesOrNo', $rules);
 }
